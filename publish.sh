0f4d755396fd2fe9a058657b6f156d096ac43a9b
Gurinder Chauhan <gurinderchauhan@gmail.com>
2026-01-17 08:51:46 -0500

    WIP

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo ""
+echo "==== INSTRUCTIONS ===="
+echo ""
+echo "Increment version number at package.json"
+echo ""
+echo "Run: vsce package"
+echo ""
+echo "File created at: <root>/copy-code-for-llm-<version>.vsix"
+echo ""
+echo "Upload file to: https://marketplace.visualstudio.com/manage/publishers/apsonex"
